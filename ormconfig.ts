4c9338f151d3421ce428adc3ef94972d470e40dd
Rodrigo Azevedo <rodrigozcma@gmail.com>
2021-04-09 22:25:54 -0300

    feat: allow ssl to database url on deployment

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,6 @@
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
+  url: `${process.env.DATABASE_URL}?ssl=true`,
   cli: {
     migrationsDir: 'src/migrations',
     entitiesDir: 'src/entity',
